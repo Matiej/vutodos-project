1a4e205ef9bdacb0df40dff56ad287bbf8fe7980
matiej <maciek.wojcik179@gmail.com>
2024-02-16 15:05:44 +0100

    first commit

---
+++ src/components/XyPapa.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    xyTitle: {
+        type: String,
+        required: false
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h2 class="green">{{ xyTitle }}</h2>
+    </div>
+</template>
